d7cb96941c7b001c2410c736657673140efb4a4f
Krish Baidya <kbaidya21@gmail.com>
2025-04-03 17:12:48 +0530

    KB - Initial spring code setup

---
+++ src/main/java/com/capstone/ecommerce/e_commerce_project/ECommerceOnlineShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.ecommerce.e_commerce_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOnlineShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOnlineShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/ecommerce/e_commerce_project/ECommerceOnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ecommerce.e_commerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
